a90ca1b5ec3ac602c5d97c18aa69946c12c9edcc
Vadim <vadimushakov44@gmail.com>
2024-06-17 20:06:58 +0300

    1 ex

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    int hours, minutes, seconds;
+
+    printf("Enter the current time (hours, minutes, seconds): \n");
+    scanf("%d %d %d", &hours, &minutes, &seconds);
+
+    if (seconds >= 30) {
+        minutes++;
+        if (minutes == 60) {
+            hours++;
+            minutes = 0;
+        }
+    }
+
+    if (hours == 24) {
+        hours = 0;
+    }
+
+    printf("Rounded current time: %d h. %d min.\n", hours, minutes);
+
+    if (minutes >= 30) {
+        hours++;
+    }
+
+    printf("Rounded current time: %d h.\n", hours);
+
     return 0;
 }
