b6d868b2716d34f72113552a3cb6a6a25370edaf
Vadim <vadimushakov44@gmail.com>
2024-06-18 11:05:32 +0300

    4 ex

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "tasks/1 task.h"
 #include "tasks/2 task.h"
 #include "tasks/3 task.h"
+#include "tasks/4 task.h"
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
@@ -12,4 +13,7 @@ int main() {
     task_2_test();
     printf("Задание 3\n");
     task_3_test();
+    printf("Задание 4\n");
+    task_4_test();
+    printf("Задание 5\n");
 }
--- tasks/4 task.h
+++ tasks/4 task.h
@@ -4,7 +4,49 @@
 #ifndef PRACTICE_4_TASK_H
 #define PRACTICE_4_TASK_H
 
+void output_array_(const float *a, const size_t n) {
+    for (size_t i = 0; i < n; i++)
+        printf("%.1f ", *(a + i));
+    printf("\n");
+}
 
+void arithmetic_mean(float *A, int n) {
+    float sum = 0;
 
+    for(int i = 0; i < n; i++) {
+        sum += A[i];
+        float average = sum / (i + 1); // Вычисляем среднее значение первых i+1 элементов
+
+        // Заменяем элемент x_i на среднее значение первых i элементов
+        A[i] = average;
+    }
+
+    output_array_(A,n);
+}
+
+void task_4_test_1() {
+    float A[]={3,6,9,12,15};
+
+    arithmetic_mean(A,5);
+}
+
+void task_4_test_2() {
+    float A[]={1,2,3,4,5};
+
+    arithmetic_mean(A,5);
+}
+
+void task_4_test_3() {
+    float A[]={1,1,1,1,1};
+
+    arithmetic_mean(A,5);
+}
+
+void task_4_test() {
+    task_4_test_1();
+    task_4_test_2();
+    task_4_test_3();
+
+}
 
 #endif //PRACTICE_4_TASK_H
